ce8ad7eab6f4ebfb2f8dfa5d395326c51101dcd2
Alex Wang <ipoopfool@gmail.com>
2018-04-27 14:54:54 -0400

    jk remove hot reloading, it doesn't preserve state. Not useful right now

--- src/index.js
+++ src/index.js
@@ -6,9 +6,3 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
-  });
-}
