f69dbc4c8cb670329786978d2ab6f9562f1de402
Alex Wang <ipoopfool@gmail.com>
2018-04-29 00:17:59 -0400

    Add CssBaseline normalizer to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Library from 'containers/Library';
+import CssBaseline from 'material-ui/CssBaseline';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Library />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <CssBaseline />
+    <Library />
+  </div>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
