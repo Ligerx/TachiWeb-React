6b48d92cf0c28d94cb1a87ffeb03941218bc6e25
Alex Wang <ipoopfool@gmail.com>
2018-04-29 22:54:16 -0400

    Library now fetches data from server

--- src/containers/Library.js
+++ src/containers/Library.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import Header from 'components/Header';
 import MangaGrid from 'components/MangaGrid';
+import TWApi from 'api';
 
 class Library extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: [],
+    };
+  }
+
+  componentDidMount() {
+    const api = TWApi.init();
+
+    api.Commands.Library.execute(
+      (res) => {
+        console.log('Success');
+        console.log(res.content);
+        this.setState({ content: res.content });
+      },
+      () => console.log('Failure'),
+    );
+  }
+
   render() {
     return (
       <div>
