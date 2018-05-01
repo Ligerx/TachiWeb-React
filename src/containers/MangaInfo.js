aeed45a9cb902ee19df9cbfee7604f217a7e1570
Alex Wang <ipoopfool@gmail.com>
2018-04-30 20:57:32 -0400

    Add some notes on todos for MangaInfo

--- src/containers/MangaInfo.js
+++ src/containers/MangaInfo.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import TWApi from 'api';
 
+// NOTES: From the previous code: When you update the server's manga info or chapter list,
+//    you should also update the client when it's complete
+
+// FEATURES TODO:
+// mark as read
+// mark as unread
+// download
+// delete
+// favorite/unfavorite
+// update info and chapters
+
 class Library extends Component {
   constructor(props) {
     super(props);
