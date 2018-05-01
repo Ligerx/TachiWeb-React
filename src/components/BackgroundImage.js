42bd1bc3b5198932817601910c3117e40ecd6e41
Alex Wang <ipoopfool@gmail.com>
2018-05-01 18:39:49 -0400

    jk now fixing merge conflict

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,8 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import { withStyles } from 'material-ui/styles';
->>>>>>> 10dbf39b34016f43a7d3a0b649a1a27f4106cfbd
 
 // TODO: I'd love to make the background image blurred, but I can't figure out how to right now.
 //       blurring the root div directly blurs EVERYTHING on top. Might not be feasible =(
