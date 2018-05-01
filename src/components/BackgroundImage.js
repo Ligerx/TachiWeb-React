5df7e8204ba6415b0a2b37c36d294baa0a5c66dc
Alex Wang <ipoopfool@gmail.com>
2018-05-01 18:36:10 -0400

    Add background image to manga info, also include title

---
+++ src/components/BackgroundImage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// TODO: I'd love to make the background image blurred, but I can't figure out how to right now.
+//       blurring the root div directly blurs EVERYTHING on top. Might not be feasible =(
+
+// * backgroundImage
+// linear-gradient hack, put a white filter over the background image
+const BackgroundImage = ({ thumbnailUrl, children }) => (
+  <div
+    style={{
+      backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0.75)), url(${thumbnailUrl})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default BackgroundImage;
--- src/components/MangaInfoDetails.js
+++ src/components/MangaInfoDetails.js
@@ -1,37 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Typography from 'material-ui/Typography';
 import ResponsiveGrid from 'components/ResponsiveGrid';
 import MangaCard from 'components/MangaCard';
 import Grid from 'material-ui/Grid';
+import BackgroundImage from 'components/BackgroundImage';
 
 // TODO: make the html a bit more DRY
 
 const MangaInfoDetails = ({ mangaInfo }) => (
   <div>
-    <ResponsiveGrid>
-      <Grid item xs={4}>
-        <MangaCard thumbnailUrl={mangaInfo.thumbnail_url} />
-      </Grid>
-      <Grid item xs={8}>
-        <Typography>
-          <strong>Author: </strong>
-          {mangaInfo.author}
-          <br />
-          <strong>Chapters: </strong>
-          {mangaInfo.chapters}
-          <br />
-          <strong>Status: </strong>
-          {mangaInfo.status}
-          <br />
-          <strong>Source: </strong>
-          {mangaInfo.source}
-          <br />
-          <strong>Genres: </strong>
-          {mangaInfo.genres}
-          <br />
-        </Typography>
-      </Grid>
-    </ResponsiveGrid>
+    <BackgroundImage thumbnailUrl={mangaInfo.thumbnail_url}>
+      <ResponsiveGrid>
+        <Grid item xs={4}>
+          <MangaCard thumbnailUrl={mangaInfo.thumbnail_url} />
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="title" gutterBottom>
+            {mangaInfo.title}
+          </Typography>
+          <Typography>
+            <strong>Author: </strong>
+            {mangaInfo.author}
+            <br />
+            <strong>Chapters: </strong>
+            {mangaInfo.chapters}
+            <br />
+            <strong>Status: </strong>
+            {mangaInfo.status}
+            <br />
+            <strong>Source: </strong>
+            {mangaInfo.source}
+            <br />
+            <strong>Genres: </strong>
+            {mangaInfo.genres}
+            <br />
+          </Typography>
+        </Grid>
+      </ResponsiveGrid>
+    </BackgroundImage>
 
     <ResponsiveGrid>
       <Typography>
