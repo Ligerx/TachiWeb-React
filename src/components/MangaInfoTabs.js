ef5fa58845c2c4b37545553bd1fc6bc633a192c5
Alex Wang <ipoopfool@gmail.com>
2018-05-01 16:39:21 -0400

    Increase tab size for MangaInfoTabs

--- src/components/MangaInfoTabs.js
+++ src/components/MangaInfoTabs.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import Paper from 'material-ui/Paper';
 import Tabs, { Tab } from 'material-ui/Tabs';
+import { withStyles } from 'material-ui/styles';
 
-const MangaInfoTabs = ({ tabValue, handleChange }) => (
-  <Paper>
-    <Tabs value={tabValue} onChange={handleChange} indicatorColor="primary" centered>
-      <Tab label="Info" />
-      <Tab label="Chapters" />
-    </Tabs>
-  </Paper>
+// TODO: I added padding to the tabs. Is this the best/right method? Will it work on small screens?
+
+const styles = () => ({
+  tab: {
+    paddingLeft: 48,
+    paddingRight: 48,
+  },
+});
+
+const MangaInfoTabs = ({ classes, tabValue, handleChange }) => (
+  <Tabs value={tabValue} onChange={handleChange} indicatorColor="primary" centered>
+    <Tab label="Info" className={classes.tab} />
+    <Tab label="Chapters" className={classes.tab} />
+  </Tabs>
 );
 
-export default MangaInfoTabs;
+export default withStyles(styles)(MangaInfoTabs);
