cb472b64052ac5371374a9ad7a6e5f211fd39147
Alex Wang <ipoopfool@gmail.com>
2018-05-04 17:41:51 -0400

    Still working on reader functionality

--- src/containers/Reader.js
+++ src/containers/Reader.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import TWApi from 'api';
-import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import Icon from 'material-ui/Icon';
 import { Link } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 // TODO: eventually create a preloading component?
 //       similar to this - https://github.com/mcarlucci/react-precache-img
@@ -10,12 +14,10 @@ import { Link } from 'react-router-dom';
 
 // TODO: If I want an <img alt="...">, I need mangaInfo, which I don't have right now.
 
-// NOTE: researching how caching works. I don't think I have to worry about what I actually render
-//       rendering can be kept separate from preloading images
-//       In fact, this might be easier than I thought. A very naive function should work just fine
-
 // TODO: FIXME: Browser doesn't seem to be pulling images from cache.
 // Not sure if that's a backend header problem, browser problem, or a local development problem
+
+// TODO: FIXME: If I switch pages really fast, the browser forcefully redownload images???
 
 // server images
 function imageUrl(mangaId, chapter, page) {
@@ -74,26 +76,60 @@ class Reader extends Component {
   render() {
     const { mangaId, chapter, page } = this.props.match.params;
 
+    const image = {
+      height: '100%',
+      backgroundImage: `url(${imageUrl(mangaId, chapter, page)})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center top',
+      backgroundSize: 'contain',
+    };
+
+    const button = {
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+    };
+
+    const test = {
+      height: '100%',
+      width: '100%',
+      position: 'absolute',
+      zIndex: 5,
+    };
+
     return (
-      <div>
-        <Button
-          variant="raised"
-          component={Link}
-          to={pageUrl(mangaId, chapter, parseInt(page, 10) - 1)}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="raised"
-          component={Link}
-          to={pageUrl(mangaId, chapter, parseInt(page, 10) + 1)}
-        >
-          Next
-        </Button>
-        <h2>{this.state.pageCount}</h2>
-        <h3>{`MangaId: ${mangaId}, Chapter: ${chapter}, Page ${page}`}</h3>
-        <img src={imageUrl(mangaId, chapter, page)} />
-      </div>
+      <React.Fragment>
+        <div style={test}>
+          <IconButton
+            component={Link}
+            to={pageUrl(mangaId, chapter, parseInt(page, 10) - 1)}
+            style={button}
+          >
+            <Icon>navigate_before</Icon>
+          </IconButton>
+          <IconButton
+            component={Link}
+            to={pageUrl(mangaId, chapter, parseInt(page, 10) + 1)}
+            style={{ ...button, right: 0 }}
+          >
+            <Icon>navigate_next</Icon>
+          </IconButton>
+
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                Title
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <h2>{this.state.pageCount}</h2>
+          <h3>{`MangaId: ${mangaId}, Chapter: ${chapter}, Page ${page}`}</h3>
+          {/* <img src={imageUrl(mangaId, chapter, page)} style={style} /> */}
+        </div>
+
+        <div style={image} />
+      </React.Fragment>
     );
   }
 }
