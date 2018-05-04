9dbdd578513d94f5d63dd0894f503dc6d925305d
Alex Wang <ipoopfool@gmail.com>
2018-05-03 20:39:27 -0400

    Begin working on Reader

---
+++ src/containers/Reader.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Reader = () => <img src="/api/img/177/727/8" />;
+
+export default Reader;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Library from 'containers/Library';
 import MangaInfo from 'containers/MangaInfo';
+import Reader from 'containers/Reader';
+
+// TODO: rename routes to make more sense
+// e.g. library    /library
+//      manga_info /:mangaId/
+//      reader     /:mangaId/:chapter/:page
 
 const Router = () => (
   <BrowserRouter>
@@ -9,6 +15,7 @@ const Router = () => (
       <Route exact path="/" component={Library} />
       <Route path="/library" component={Library} />
       <Route path="/manga_info/:mangaId" component={MangaInfo} />
+      <Route path="/reader/:mangaId/:chapter/:page" component={Reader} />
     </Switch>
   </BrowserRouter>
 );
