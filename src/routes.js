74e486da0dca0006f1493a3091e292063c94426a
Alex Wang <ipoopfool@gmail.com>
2018-05-05 20:21:07 -0400

    Changed up the routes

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,13 @@ import LibraryContainer from 'containers/LibraryContainer';
 import MangaInfo from 'containers/MangaInfo';
 import Reader from 'containers/Reader';
 
-// TODO: rename routes to make more sense
-// e.g. library    /library
-//      manga_info /:mangaId/
-//      reader     /:mangaId/:chapter/:page
-
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={LibraryContainer} />
       <Route path="/library" component={LibraryContainer} />
-      <Route path="/manga_info/:mangaId" component={MangaInfo} />
-      <Route path="/reader/:mangaId/:chapterId/:page" component={Reader} />
+      <Route path="/:mangaId/:chapterId/:page" component={Reader} />
+      <Route path="/:mangaId" component={MangaInfo} />
     </Switch>
   </BrowserRouter>
 );
