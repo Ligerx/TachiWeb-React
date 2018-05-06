634d98c6bbcaaa209b8e648a8d94984be41a63c2
Alex Wang <ipoopfool@gmail.com>
2018-05-06 18:19:53 -0400

    Fix chapters not being sent correctly to mangaInfo

--- src/containers/MangaInfoContainer.js
+++ src/containers/MangaInfoContainer.js
@@ -3,22 +3,20 @@ import { fetchLibrary } from 'redux-ducks/library';
 import { fetchChapters } from 'redux-ducks/chapters';
 import MangaInfo from 'containers/MangaInfo';
 
-// search the manga library for the manga that matches the url param
+const getThisManga = (mangaLibrary, mangaId) =>
+  mangaLibrary.find(manga => manga.id === parseInt(mangaId, 10)) || {};
 
-// https://stackoverflow.com/questions/44725802/within-mapstatetoprops-how-to-obtain-a-react-router-param?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+const mapStateToProps = (state, ownProps) => {
+  const { library } = state;
+  const { chapters } = state.chapters;
+  const { mangaId } = ownProps.match.params;
 
-// FIXME: before library is fetched, can't find this manga, so can't display text
-//        the correct solution would be to do a do a loading spinner state instead of rendering
-const getThisManga = (mangaLibrary, mangaId) => {
-  console.log('test');
-  return mangaLibrary.find(manga => manga.id === parseInt(mangaId, 10)) || {};
+  return {
+    mangaInfo: getThisManga(library.mangaLibrary, mangaId),
+    chapters: chapters[mangaId],
+    mangaInfoIsFetching: library.isFetching,
+  };
 };
-
-const mapStateToProps = (state, ownProps) => ({
-  mangaInfo: getThisManga(state.library.mangaLibrary, ownProps.match.params.mangaId),
-  chapters: state.chapters.chapters,
-  mangaInfoIsFetching: state.library.isFetching,
-});
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   fetchLibrary: () => dispatch(fetchLibrary()),
