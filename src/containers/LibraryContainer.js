4a7d579ddc2aacc3dbf70167aff43c5bcdf1aa0e
Alex Wang <ipoopfool@gmail.com>
2018-05-06 17:02:27 -0400

    Filter library to only show favorited manga

--- src/containers/LibraryContainer.js
+++ src/containers/LibraryContainer.js
@@ -2,8 +2,10 @@ import { connect } from 'react-redux';
 import { fetchLibrary } from 'redux-ducks/library';
 import Library from 'containers/Library';
 
+// mangaLibrary - it's possible to have manga stored but not favorited. So filter that out.
+//                e.g. unfavoriting something
 const mapStateToProps = state => ({
-  mangaLibrary: state.library.mangaLibrary,
+  mangaLibrary: state.library.mangaLibrary.filter(manga => manga.favorite),
 });
 
 const mapDispatchToProps = dispatch => ({
