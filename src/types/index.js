32a4c12a94ff3bca0b81b1c0585eb1c7ef40cc56
Alex Wang <ipoopfool@gmail.com>
2018-05-06 19:18:20 -0400

    Create types to use for PropType validation

---
+++ src/types/index.js
@@ -0,0 +1,40 @@
+// Basically schemas for data retrieved from the server
+// Based on the below link
+// https://medium.freecodecamp.org/react-pattern-centralized-proptypes-f981ff672f3b
+
+import { shape, number, string, bool, array } from 'prop-types';
+
+export const mangaType = shape({
+  chapters: number.isRequired,
+  unread: number.isRequired,
+  author: string.isRequired,
+  flags: shape({
+    DISPLAY_MODE: string.isRequired,
+    READ_FILTER: string.isRequired,
+    SORT_DIRECTION: string.isRequired,
+    SORT_TYPE: string.isRequired,
+    DOWNLOADED_FILTER: string.isRequired,
+  }),
+  description: string.isRequired,
+  source: string.isRequired,
+  title: string.isRequired,
+  thumbnail_url: string.isRequired,
+  downloaded: bool.isRequired,
+  url: string.isRequired,
+  genres: string.isRequired,
+  id: number.isRequired,
+  categories: array.isRequired,
+  favorite: bool.isRequired,
+  status: string.isRequired,
+});
+
+export const chapterType = shape({
+  date: number.isRequired,
+  source_order: number.isRequired,
+  read: bool.isRequired,
+  name: string.isRequired,
+  chapter_number: number.isRequired,
+  download_status: string.isRequired,
+  id: number.isRequired,
+  last_page_read: number.isRequired,
+});
