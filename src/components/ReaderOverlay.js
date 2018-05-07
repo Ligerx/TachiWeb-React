434051d3615a4e9ee5686e338eded07d5feae819
Alex Wang <ipoopfool@gmail.com>
2018-05-07 19:22:01 -0400

    ReaderOverlay is now only visible on hover

--- src/components/ReaderOverlay.js
+++ src/components/ReaderOverlay.js
@@ -8,48 +8,50 @@ import Icon from 'material-ui/Icon';
 import IconButton from 'material-ui/IconButton';
 import { Client } from 'api';
 import PageSlider from 'components/PageSlider';
+import { withStyles } from 'material-ui/styles';
 
-// ******** What pieces of data do I need? ********
-// mangaInfo.title
-// chapter.chapter_number plus some basic processing
-// pageCount
-// page (from url params)
-
-// TODO: move these styles out to a withStyles wrapper
 // TODO: using two toolbars currently, but it might be too big. Consider changing/customizing later.
 // NOTE: Material-UI v1 hasn't ported a slider component yet, so use an external library.
 //       When it is added to Material-UI, use that instead.
 //       https://github.com/mui-org/material-ui/issues/4793
-const ReaderOverlay = ({
-  title, chapterNum, pageCount, mangaId,
-}) => {
-  // NOTE: removed match from the above, may need to add it back?
-  const overlay = {
+
+const styles = {
+  overlay: {
+    // Overlay it above the image
     width: '100%',
     position: 'absolute',
     zIndex: 1,
-  };
 
-  // FIXME: slider defaultValue not working???
-  return (
-    <div style={overlay}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <IconButton component={Link} to={Client.manga(mangaId)}>
-            <Icon>arrow_back</Icon>
-          </IconButton>
-          <Typography variant="title" style={{ flex: 1 }}>
-            {title}
-          </Typography>
-          <Typography variant="subheading"> Chapter {chapterNumFormatter(chapterNum)}</Typography>
-        </Toolbar>
-        <Toolbar>
-          <PageSlider pageCount={pageCount} />
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    // Visible only on hover
+    opacity: 0,
+    transition: 'opacity .2s ease-in-out',
+    '&:hover': {
+      opacity: 1,
+    },
+  },
 };
+
+// NOTE: removed match from the destructured props, may need to add it back?
+const ReaderOverlay = ({
+  title, chapterNum, pageCount, mangaId, classes,
+}) => (
+  <div className={classes.overlay}>
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <IconButton component={Link} to={Client.manga(mangaId)}>
+          <Icon>arrow_back</Icon>
+        </IconButton>
+        <Typography variant="title" style={{ flex: 1 }}>
+          {title}
+        </Typography>
+        <Typography variant="subheading"> Chapter {chapterNumFormatter(chapterNum)}</Typography>
+      </Toolbar>
+      <Toolbar>
+        <PageSlider pageCount={pageCount} />
+      </Toolbar>
+    </AppBar>
+  </div>
+);
 
 // Helper Function
 function chapterNumFormatter(chapterNum) {
@@ -67,6 +69,8 @@ ReaderOverlay.propTypes = {
   chapterNum: PropTypes.number.isRequired,
   pageCount: PropTypes.number.isRequired,
   mangaId: PropTypes.number.isRequired,
+  // classes is the injected styles
+  classes: PropTypes.object.isRequired,
   // match: PropTypes.shape({
   //   params: PropTypes.shape({
   //     page: PropTypes.string.isRequired,
@@ -74,4 +78,4 @@ ReaderOverlay.propTypes = {
   // }).isRequired,
 };
 
-export default ReaderOverlay;
+export default withStyles(styles)(ReaderOverlay);
