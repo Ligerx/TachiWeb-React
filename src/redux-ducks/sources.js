8d74850fc3a53386c03ab209286f7524cfc15a74
Alex Wang <ipoopfool@gmail.com>
2018-05-10 00:24:53 -0400

    Bugfix sources duck, and rename variable

--- src/redux-ducks/sources.js
+++ src/redux-ducks/sources.js
@@ -1,13 +1,13 @@
 import { Server } from 'api';
 
 // Actions
-const REQUEST = 'chapters/LOAD_REQUEST';
-const SUCCESS = 'chapters/LOAD_SUCCESS';
-const FAILURE = 'chapters/LOAD_FAILURE';
+const REQUEST = 'sources/LOAD_REQUEST';
+const SUCCESS = 'sources/LOAD_SUCCESS';
+const FAILURE = 'sources/LOAD_FAILURE';
 
 // Reducers
 export default function sourcesReducer(
-  state = { sources: [], isFetching: false, error: false },
+  state = { sourcesArray: [], isFetching: false, error: false },
   action = {},
 ) {
   switch (action.type) {
@@ -16,7 +16,7 @@ export default function sourcesReducer(
     case SUCCESS:
       return {
         ...state,
-        sources: [...state.sources, ...action.payload],
+        sourcesArray: [...state.sourcesArray, ...action.payload],
         isFetching: false,
       };
     case FAILURE:
@@ -33,6 +33,6 @@ export function fetchSources() {
 
     return fetch(Server.sources())
       .then(res => res.json(), error => dispatch({ type: FAILURE, payload: error }))
-      .then(json => dispatch({ type: SUCCESS, payload: json.contents }));
+      .then(json => dispatch({ type: SUCCESS, payload: json.content }));
   };
 }
