c473078581d29df308b4c2917f15d62e83a53028
Alex Wang <ipoopfool@gmail.com>
2018-05-22 17:57:32 -0400

    Update types for flow (untested)

--- src/types/index.js
+++ src/types/index.js
@@ -1,47 +1,51 @@
-// Basically schemas for data retrieved from the server
-// Based on the below link
-// https://medium.freecodecamp.org/react-pattern-centralized-proptypes-f981ff672f3b
-
-import { shape, number, string, bool, array } from 'prop-types';
-
-export const mangaType = shape({
+export type MangaType = {
   // NOTE: Many non-required fields may be missing because the server needs time to
   //       scrape the website, but returns a barebones object early anyway.
 
   // Must be included
-  id: number.isRequired,
-  favorite: bool.isRequired,
-  title: string.isRequired,
+  id: string,
+  favorite: boolean,
+  title: string,
 
   // I believe these will always be incliuded
-  source: string.isRequired,
-  url: string.isRequired,
-  downloaded: bool.isRequired,
-  flags: shape({
-    DISPLAY_MODE: string.isRequired,
-    READ_FILTER: string.isRequired,
-    SORT_DIRECTION: string.isRequired,
-    SORT_TYPE: string.isRequired,
-    DOWNLOADED_FILTER: string.isRequired,
-  }),
+  source: string,
+  url: string,
+  downloaded: boolean,
+  flags: {
+    DISPLAY_MODE: string,
+    READ_FILTER: string,
+    SORT_DIRECTION: string,
+    SORT_TYPE: string,
+    DOWNLOADED_FILTER: string,
+  },
 
-  chapters: number,
-  unread: number,
-  author: string,
-  description: string,
-  thumbnail_url: string,
-  genres: string,
-  categories: array,
-  status: string,
-});
+  chapters: ?number,
+  unread: ?number,
+  author: ?string,
+  description: ?string,
+  thumbnail_url: ?string,
+  genres: ?string,
+  categories: ?(string[]),
+  status: ?string,
+};
 
-export const chapterType = shape({
-  date: number.isRequired,
-  source_order: number.isRequired,
-  read: bool.isRequired,
-  name: string.isRequired,
-  chapter_number: number.isRequired,
-  download_status: string.isRequired,
-  id: number.isRequired,
-  last_page_read: number.isRequired,
-});
+export type ChapterType = {
+  date: number,
+  source_order: number,
+  read: boolean,
+  name: string,
+  chapter_number: number,
+  download_status: string,
+  id: number,
+  last_page_read: number,
+};
+
+export type SourceType = {
+  name: string,
+  supports_latest: boolean,
+  id: number,
+  lang: {
+    name: string,
+    display_name: string,
+  },
+};
