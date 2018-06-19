d5d3f2a3acacb8ab7670b73f3b1bd433df408199
Alex Wang <ipoopfool@gmail.com>
2018-06-19 15:41:53 -0400

    Implement restore flow.Use a container with RestoreCard, remove container from BackupRestore.Rename uploadRestoreData to uploadRestoreFile.Add createErrorMessageSelector to error duck

--- src/pages/BackupRestore.js
+++ src/pages/BackupRestore.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -7,44 +7,32 @@ import MenuDrawer from 'components/MenuDrawer';
 import ResponsiveGrid from 'components/ResponsiveGrid';
 import { Grid } from '@material-ui/core';
 import BackupCard from 'components/backup-restore/BackupCard';
-import RestoreCard from 'components/backup-restore/RestoreCard';
-import type { BackupRestoreContainerProps as Props } from 'containers/BackupRestoreContainer';
+import RestoreCardContainer from 'containers/RestoreCardContainer';
 import { Helmet } from 'react-helmet';
 
-// TODO: Use a very small max width for the responsive grid in this page
-//       will require updating ResponsiveGrid's code
+const BackupRestore = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Backup and Restore - TachiWeb</title>
+    </Helmet>
 
-class BackupRestore extends Component<Props> {
-  handleUploadRestore = (file: File) => {
-    this.props.uploadRestoreData(file);
-  };
+    <AppBar color="default" position="static" style={{ marginBottom: 20 }}>
+      <Toolbar>
+        <MenuDrawer />
+        <Typography variant="title">Backup / Restore</Typography>
+      </Toolbar>
+    </AppBar>
 
-  render() {
-    return (
-      <React.Fragment>
-        <Helmet>
-          <title>Backup and Restore - TachiWeb</title>
-        </Helmet>
+    <ResponsiveGrid maxWidth="xs">
+      <Grid item xs={12}>
+        <BackupCard />
+      </Grid>
 
-        <AppBar color="default" position="static" style={{ marginBottom: 20 }}>
-          <Toolbar>
-            <MenuDrawer />
-            <Typography variant="title">Backup / Restore</Typography>
-          </Toolbar>
-        </AppBar>
-
-        <ResponsiveGrid maxWidth="xs">
-          <Grid item xs={12}>
-            <BackupCard />
-          </Grid>
-
-          <Grid item xs={12}>
-            <RestoreCard onClickRestore={this.handleUploadRestore} />
-          </Grid>
-        </ResponsiveGrid>
-      </React.Fragment>
-    );
-  }
-}
+      <Grid item xs={12}>
+        <RestoreCardContainer />
+      </Grid>
+    </ResponsiveGrid>
+  </React.Fragment>
+);
 
 export default BackupRestore;
