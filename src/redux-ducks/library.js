84e3f709cf770c77fc44df8e7cd48b77a54ea2be
Alex Wang <ipoopfool@gmail.com>
2019-03-05 22:24:18 -0800

    Update library duck state schema, default state. Add flags fetch and set. Clean up useless actions

--- src/redux-ducks/library.js
+++ src/redux-ducks/library.js
@@ -1,37 +1,42 @@
 // @flow
-import { Server } from 'api';
-import type { LibraryFlagsType } from 'types';
-import { ADD_MANGA } from './mangaInfos';
-import { handleHTMLError, transformToMangaIdsArray } from './utils';
+import { Server } from "api";
+import type { LibraryFlagsType } from "types";
+import { ADD_MANGA } from "./mangaInfos";
+import { handleHTMLError, transformToMangaIdsArray } from "./utils";
 
 // ================================================================================
 // Actions
 // ================================================================================
-const FETCH_LIBRARY_REQUEST = 'library/FETCH_REQUEST';
-const FETCH_LIBRARY_SUCCESS = 'library/FETCH_SUCCESS';
-const FETCH_LIBRARY_FAILURE = 'library/FETCH_FAILURE';
-const FETCH_LIBRARY_CACHE = 'library/FETCH_CACHE';
-export const FETCH_LIBRARY = 'library/FETCH';
+const FETCH_LIBRARY_REQUEST = "library/FETCH_REQUEST";
+const FETCH_LIBRARY_SUCCESS = "library/FETCH_SUCCESS";
+const FETCH_LIBRARY_FAILURE = "library/FETCH_FAILURE";
+const FETCH_LIBRARY_CACHE = "library/FETCH_CACHE";
+export const FETCH_LIBRARY = "library/FETCH";
 
-const FETCH_UNREAD_REQUEST = 'library/FETCH_UNREAD_REQUEST';
-const FETCH_UNREAD_SUCCESS = 'library/FETCH_UNREAD_SUCCESS';
-const FETCH_UNREAD_FAILURE = 'library/FETCH_UNREAD_FAILURE';
-const FETCH_UNREAD_CACHE = 'library/FETCH_UNREAD_CACHE';
-export const FETCH_UNREAD = 'library/FETCH_UNREAD';
+const FETCH_UNREAD_REQUEST = "library/FETCH_UNREAD_REQUEST";
+const FETCH_UNREAD_SUCCESS = "library/FETCH_UNREAD_SUCCESS";
+const FETCH_UNREAD_FAILURE = "library/FETCH_UNREAD_FAILURE";
+const FETCH_UNREAD_CACHE = "library/FETCH_UNREAD_CACHE";
+export const FETCH_UNREAD = "library/FETCH_UNREAD";
 
-export const ADD_TO_FAVORITES = 'library/ADD_TO_FAVORITES';
-export const REMOVE_FROM_FAVORITES = 'library/REMOVE_FROM_FAVORITES';
-export const ADJUST_UNREAD = 'library/ADJUST_UNREAD';
+export const ADD_TO_FAVORITES = "library/ADD_TO_FAVORITES";
+export const REMOVE_FROM_FAVORITES = "library/REMOVE_FROM_FAVORITES";
+export const ADJUST_UNREAD = "library/ADJUST_UNREAD";
 
-const UPLOAD_RESTORE_REQUEST = 'library/UPLOAD_RESTORE_REQUEST';
-const UPLOAD_RESTORE_SUCCESS = 'library/UPLOAD_RESTORE_SUCCESS';
-const UPLOAD_RESTORE_FAILURE = 'library/UPLOAD_RESTORE_FAILURE';
-export const UPLOAD_RESTORE = 'library/UPLOAD_RESTORE';
+const UPLOAD_RESTORE_REQUEST = "library/UPLOAD_RESTORE_REQUEST";
+const UPLOAD_RESTORE_SUCCESS = "library/UPLOAD_RESTORE_SUCCESS";
+const UPLOAD_RESTORE_FAILURE = "library/UPLOAD_RESTORE_FAILURE";
+export const UPLOAD_RESTORE = "library/UPLOAD_RESTORE";
 
-const SET_FLAG_REQUEST = 'library/SET_FLAG_REQUEST';
-const SET_FLAG_SUCCESS = 'library/SET_FLAG_SUCCESS';
-const SET_FLAG_FAILURE = 'library/SET_FLAG_FAILURE';
-const SET_FLAG_NO_CHANGE = 'library/SET_FLAG_NO_CHANGE';
+const FETCH_LIBRARY_FLAGS_REQUEST = "library/FETCH_FLAGS_REQUEST";
+const FETCH_LIBRARY_FLAGS_SUCCESS = "library/FETCH_FLAGS_SUCCESS";
+const FETCH_LIBRARY_FLAGS_FAILURE = "library/FETCH_FLAGS_FAILURE";
+const FETCH_LIBRARY_FLAGS_CACHE = "library/FETCH_FLAGS_CACHE";
+export const FETCH_LIBRARY_FLAGS = "library/FETCH_FLAGS";
+
+const SET_FLAG_REQUEST = "library/SET_FLAG_REQUEST";
+const SET_FLAG_SUCCESS = "library/SET_FLAG_SUCCESS";
+const SET_FLAG_FAILURE = "library/SET_FLAG_FAILURE";
 
 // ================================================================================
 // Reducers
@@ -42,57 +47,72 @@ type State = {
   +unread: { +[mangaId: number]: number },
   +reloadUnread: boolean,
   +flags: LibraryFlagsType,
+  +isFlagsLoaded: boolean
+};
+
+const defaultState: State = {
+  mangaIds: [], // array of mangaIds that point at data loaded in mangaInfos reducer
+  reloadLibrary: true, // Library should be loaded once on first visit
+  unread: {}, // { mangaId: int }
+  reloadUnread: true, // should refresh unread for library if something new is added
+  flags: {
+    filters: [
+      {
+        type: "DOWNLOADED",
+        status: "ANY"
+      },
+      {
+        type: "UNREAD",
+        status: "ANY"
+      },
+      {
+        type: "COMPLETED",
+        status: "ANY"
+      }
+    ],
+    sort: {
+      type: "ALPHA",
+      direction: "ASCENDING"
+    },
+    display: "GRID",
+    show_download_badges: false
+  },
+  isFlagsLoaded: false // fetch flags on first load
 };
 
 export default function libraryReducer(
-  state: State = {
-    mangaIds: [], // array of mangaIds that point that data loaded in mangaInfos reducer
-    reloadLibrary: true, // Library should be loaded once on first visit
-    unread: {}, // { mangaId: int }
-    reloadUnread: true, // should refresh unread for library if something new is added
-    flags: {
-      DOWNLOADED_FILTER: 'ALL',
-      READ_FILTER: 'ALL',
-      COMPLETED_FILTER: 'ALL',
-      SORT_TYPE: 'ALPHABETICALLY',
-      SORT_DIRECTION: 'ASCENDING',
-    },
-  },
-  action = {},
+  state: State = defaultState,
+  action = {}
 ) {
   switch (action.type) {
     case FETCH_LIBRARY_SUCCESS:
       return {
         ...state,
         mangaIds: action.mangaIds,
-        reloadLibrary: false,
+        reloadLibrary: false
       };
-
-    case FETCH_LIBRARY_CACHE:
-      return state;
 
     case FETCH_UNREAD_SUCCESS:
       return {
         ...state,
         unread: action.unread,
-        reloadUnread: false,
+        reloadUnread: false
       };
-
-    case FETCH_UNREAD_CACHE:
-      return state;
 
     case ADD_TO_FAVORITES:
       return {
         ...state,
         mangaIds: [...state.mangaIds, action.mangaId],
-        reloadUnread: true,
+        reloadUnread: true
       };
 
     case REMOVE_FROM_FAVORITES: {
-      const newMangaIds: Array<number> = state.mangaIds.filter(mangaId => mangaId !== action.mangaId);
+      const newMangaIds: Array<number> = state.mangaIds.filter(
+        mangaId => mangaId !== action.mangaId
+      );
       return {
         ...state,
-        mangaIds: newMangaIds,
+        mangaIds: newMangaIds
       };
     }
 
@@ -103,25 +123,33 @@ export default function libraryReducer(
         ...state,
         unread: {
           ...unread,
-          [mangaId]: unread[mangaId] + difference,
-        },
+          [mangaId]: unread[mangaId] + difference
+        }
       };
     }
-
-    case SET_FLAG_REQUEST:
-      return {
-        ...state,
-        flags: {
-          ...state.flags,
-          [action.flag]: action.state,
-        },
-      };
 
     case UPLOAD_RESTORE_SUCCESS:
       return {
         ...state,
         reloadLibrary: true,
         reloadUnread: true,
+        isFlagsLoaded: false
+      };
+
+    case FETCH_LIBRARY_FLAGS_SUCCESS:
+      return {
+        ...state,
+        flags: action.flags,
+        isFlagsLoaded: true
+      };
+
+    case SET_FLAG_REQUEST:
+      return {
+        ...state,
+        flags: {
+          ...state.flags,
+          [action.flag]: action.state
+        }
       };
 
     default:
@@ -145,7 +173,7 @@ export function fetchLibrary({ ignoreCache = false }: Options = {}) {
     return fetch(Server.library())
       .then(handleHTMLError)
       .then(
-        (json) => {
+        json => {
           const { content } = json;
           const mangaIds = transformToMangaIdsArray(content);
 
@@ -155,9 +183,9 @@ export function fetchLibrary({ ignoreCache = false }: Options = {}) {
         error =>
           dispatch({
             type: FETCH_LIBRARY_FAILURE,
-            errorMessage: 'Failed to load your library',
-            meta: { error },
-          }),
+            errorMessage: "Failed to load your library",
+            meta: { error }
+          })
       );
   };
 }
@@ -173,13 +201,17 @@ export function fetchUnread({ ignoreCache = false }: Options = {}) {
     return fetch(Server.libraryUnread())
       .then(handleHTMLError)
       .then(
-        json => dispatch({ type: FETCH_UNREAD_SUCCESS, unread: transformUnread(json.content) }),
+        json =>
+          dispatch({
+            type: FETCH_UNREAD_SUCCESS,
+            unread: transformUnread(json.content)
+          }),
         error =>
           dispatch({
             type: FETCH_UNREAD_FAILURE,
-            errorMessage: 'Failed to get unread chapters for your library',
-            meta: { error },
-          }),
+            errorMessage: "Failed to get unread chapters for your library",
+            meta: { error }
+          })
       );
   };
 }
@@ -191,28 +223,56 @@ export function uploadRestoreFile(file: File) {
     return fetch(Server.restoreUpload(), uploadPostParameters(file))
       .then(handleHTMLError)
       .then(
-        // TODO: I'm not currently if the response message says failure or success
+        // TODO: I'm not currently checking if the response message says failure or success
         () => dispatch({ type: UPLOAD_RESTORE_SUCCESS }),
         error =>
           dispatch({
             type: UPLOAD_RESTORE_FAILURE,
             errorMessage: `Failed to restore library from ${file.name}`,
-            meta: { error },
-          }),
+            meta: { error }
+          })
+      );
+  };
+}
+
+export function fetchLibraryFlags() {
+  return (dispatch: Function, getState: Function) => {
+    if (getState().library.isFlagsLoaded) {
+      return dispatch({ type: FETCH_LIBRARY_FLAGS_CACHE });
+    }
+
+    dispatch({ type: FETCH_LIBRARY_FLAGS_REQUEST });
+
+    return fetch(Server.libraryFlags())
+      .then(handleHTMLError)
+      .then(
+        json =>
+          dispatch({ type: FETCH_LIBRARY_FLAGS_SUCCESS, flags: json.data }),
+        error =>
+          dispatch({
+            type: FETCH_LIBRARY_FLAGS_FAILURE,
+            errorMessage: "Failed to load your library settings.",
+            meta: { error }
+          })
       );
   };
 }
 
 export function setLibraryFlag(flag: string, state: string) {
-  // TODO: there are no library flags on the server yet, so this is all local
-  //       Update this function when the server eventually stores this data.
+  // Updating the store without waiting for the server to reply
   return (dispatch: Function, getState: Function) => {
-    if (getState().library.flags[flag] === state) {
-      return dispatch({ type: SET_FLAG_NO_CHANGE, meta: { flag, state } });
-    }
-
     dispatch({ type: SET_FLAG_REQUEST, flag, state });
-    return dispatch({ type: SET_FLAG_SUCCESS });
+
+    return fetch(Server.libraryFlags(), {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(getState().library.flags)
+    })
+      .then(handleHTMLError)
+      .then(
+        () => dispatch({ type: SET_FLAG_SUCCESS }),
+        () => dispatch({ SET_FLAG_FAILURE })
+      );
   };
 }
 
@@ -224,7 +284,7 @@ type Return = { [mangaId: number]: number };
 
 function transformUnread(unreadArray: Param): Return {
   const newUnread = {};
-  unreadArray.forEach((unreadObj) => {
+  unreadArray.forEach(unreadObj => {
     newUnread[unreadObj.id] = unreadObj.unread;
   });
   return newUnread;
@@ -232,7 +292,7 @@ function transformUnread(unreadArray: Param): Return {
 
 function uploadPostParameters(file: File): Object {
   const formData = new FormData();
-  formData.append('uploaded_file', file);
+  formData.append("uploaded_file", file);
 
-  return { method: 'POST', body: formData };
+  return { method: "POST", body: formData };
 }
