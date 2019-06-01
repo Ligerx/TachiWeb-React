fd3f85e19dc32dfad4a2fddd2ef0f4d1811aadf1
Alex Wang <ipoopfool@gmail.com>
2019-06-01 10:31:35 -0700

    just prettier error notifications

--- src/containers/ErrorNotificationsContainer.js
+++ src/containers/ErrorNotificationsContainer.js
@@ -1,10 +1,10 @@
 // @flow
-import { connect } from 'react-redux';
-import ErrorNotifications from 'components/ErrorNotifications';
-import { allErrorsSelector } from 'redux-ducks/error';
+import { connect } from "react-redux";
+import ErrorNotifications from "components/ErrorNotifications";
+import { allErrorsSelector } from "redux-ducks/error";
 
 const mapStateToProps = state => ({
-  errorMessage: allErrorsSelector(state),
+  errorMessage: allErrorsSelector(state)
 });
 
 export default connect(mapStateToProps)(ErrorNotifications);
