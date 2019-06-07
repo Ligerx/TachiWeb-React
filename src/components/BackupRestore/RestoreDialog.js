873395cad327695fd6c3ae0cf9a40b5298510e27
Alex Wang <ipoopfool@gmail.com>
2019-06-06 19:33:25 -0700

    Move Restore redux down

--- src/components/BackupRestore/RestoreDialog.js
+++ src/components/BackupRestore/RestoreDialog.js
@@ -1,40 +1,45 @@
 // @flow
-import React, { Component } from 'react';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import { withStyles } from '@material-ui/core/styles';
-import CenteredLoading from 'components/Loading/CenteredLoading';
-import DialogActions from '@material-ui/core/DialogActions';
-import Button from '@material-ui/core/Button';
-import { Client } from 'api';
-import Link from 'components/Link';
+import React from "react";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import CenteredLoading from "components/Loading/CenteredLoading";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import { Client } from "api";
+import Link from "components/Link";
+import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/styles";
+import {
+  selectIsRestoreLoading,
+  selectDidRestoreFail
+} from "redux-ducks/library";
 
-const styles = {
+const useStyles = makeStyles({
   dialog: {
     width: 320,
-    height: 126, // TODO: using an arbitrary height for all dialog states. Make this less bad.
-  },
-};
+    height: 126 // TODO: using an arbitrary height for all dialog states. Make this less bad.
+  }
+});
 
 type Props = {
-  classes: Object, // injected styles
   open: boolean,
   onClose: Function,
-  isLoading: boolean,
-  failed: boolean,
-  tryAgain: Function,
+  tryAgain: Function
 };
 
-class RestoreDialog extends Component<Props> {
-  handleClose = () => {
-    if (this.props.isLoading) return;
-    this.props.onClose();
+const RestoreDialog = ({ open, onClose, tryAgain }: Props) => {
+  const classes = useStyles();
+
+  const isRestoreLoading = useSelector(selectIsRestoreLoading);
+  const didRestoreFail = useSelector(selectDidRestoreFail);
+
+  const handleClose = () => {
+    if (isRestoreLoading) return;
+    onClose();
   };
 
-  content = () => {
-    const { isLoading, failed, tryAgain } = this.props;
-
-    if (isLoading) {
+  const content = () => {
+    if (isRestoreLoading) {
       return (
         <React.Fragment>
           <DialogTitle>Restoring Library...</DialogTitle>
@@ -43,12 +48,12 @@ class RestoreDialog extends Component<Props> {
       );
     }
 
-    if (failed) {
+    if (didRestoreFail) {
       return (
         <React.Fragment>
           <DialogTitle>Failed to Restore Library</DialogTitle>
           <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
+            <Button onClick={handleClose} color="primary">
               Cancel
             </Button>
             <Button onClick={tryAgain} color="primary" autoFocus>
@@ -63,10 +68,15 @@ class RestoreDialog extends Component<Props> {
       <React.Fragment>
         <DialogTitle>Restore Successful</DialogTitle>
         <DialogActions>
-          <Button onClick={this.handleClose} color="primary">
+          <Button onClick={handleClose} color="primary">
             Close
           </Button>
-          <Button component={Link} to={Client.library()} color="primary" autoFocus>
+          <Button
+            component={Link}
+            to={Client.library()}
+            color="primary"
+            autoFocus
+          >
             Go To Library
           </Button>
         </DialogActions>
@@ -74,15 +84,15 @@ class RestoreDialog extends Component<Props> {
     );
   };
 
-  render() {
-    const { classes, open } = this.props;
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      classes={{ paper: classes.dialog }}
+    >
+      {content()}
+    </Dialog>
+  );
+};
 
-    return (
-      <Dialog open={open} onClose={this.handleClose} classes={{ paper: classes.dialog }}>
-        {this.content()}
-      </Dialog>
-    );
-  }
-}
-
-export default withStyles(styles)(RestoreDialog);
+export default RestoreDialog;
