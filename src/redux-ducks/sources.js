71e09a1e58b407d63846b2ef20ccde9716c89882
Alex Wang <ipoopfool@gmail.com>
2019-06-09 23:45:01 -0700

    Update typing for sources

--- src/redux-ducks/sources.js
+++ src/redux-ducks/sources.js
@@ -9,16 +9,34 @@ import { selectCatalogueSourceId, changeSourceId } from "redux-ducks/catalogue";
 // Actions
 // ================================================================================
 const FETCH_REQUEST = "sources/FETCH_REQUEST";
+type FETCH_REQUEST_TYPE = "sources/FETCH_REQUEST";
 const FETCH_SUCCESS = "sources/FETCH_SUCCESS";
+type FETCH_SUCCESS_TYPE = "sources/FETCH_SUCCESS";
 const FETCH_FAILURE = "sources/FETCH_FAILURE";
-export const FETCH_SOURCES = "sources/FETCH";
+type FETCH_FAILURE_TYPE = "sources/FETCH_FAILURE";
+const FETCH_SOURCES = "sources/FETCH";
+
+type FetchRequestAction = { type: FETCH_REQUEST_TYPE };
+type FetchSuccessAction = {
+  type: FETCH_SUCCESS_TYPE,
+  payload: $ReadOnlyArray<SourceType>
+};
+type FetchFailureAction = {
+  type: FETCH_FAILURE_TYPE,
+  errorMessage: string,
+  meta: Object
+};
 
 // ================================================================================
 // Reducers
 // ================================================================================
 type State = $ReadOnlyArray<SourceType>;
+type Action = FetchRequestAction | FetchSuccessAction | FetchFailureAction;
 
-export default function sourcesReducer(state: State = [], action = {}) {
+export default function sourcesReducer(
+  state: State = [],
+  action: Action
+): State {
   switch (action.type) {
     case FETCH_SUCCESS:
       return action.payload;
@@ -37,8 +55,16 @@ export const selectSources = (state): Array<SourceType> => state.sources;
 // ================================================================================
 // Action Creators
 // ================================================================================
-export function fetchSources() {
-  return (dispatch: Function, getState: Function) => {
+type GetState = () => State;
+type PromiseAction = Promise<Action>;
+// eslint-disable-next-line no-use-before-define
+type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
+type Dispatch = (
+  action: Action | ThunkAction | PromiseAction | Array<Action>
+) => any;
+
+export function fetchSources(): ThunkAction {
+  return (dispatch, getState) => {
     dispatch({ type: FETCH_REQUEST });
 
     return fetch(Server.sources())
