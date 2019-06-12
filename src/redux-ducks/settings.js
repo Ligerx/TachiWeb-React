1850caa92f7f201a0eb349327586aa4cfd3f3260
Alex Wang <ipoopfool@gmail.com>
2019-06-11 19:01:01 -0700

    Type settings redux

--- src/redux-ducks/settings.js
+++ src/redux-ducks/settings.js
@@ -3,39 +3,103 @@ import { Server } from "api";
 import type { SchemaType } from "types/settings-schema";
 import UI_SETTINGS from "ui-settings";
 import { createLoadingSelector } from "redux-ducks/loading";
+import type { GlobalState } from "redux-ducks/reducers";
+
+export type PrefValue = string | Array<string> | number | boolean | null | void;
+export type PrefsType = $ReadOnly<{ [key: string]: PrefValue }>;
 
 // ================================================================================
 // Actions
 // ================================================================================
-export const FETCH_PREFS = "settings/FETCH";
-const FETCH_PREFS_REQUEST = `${FETCH_PREFS}_REQUEST`;
-const FETCH_PREFS_SUCCESS = `${FETCH_PREFS}_SUCCESS`;
-const FETCH_PREFS_FAILURE = `${FETCH_PREFS}_FAILURE`;
-const FETCH_PREFS_CACHE = `${FETCH_PREFS}_CACHE`;
+const FETCH_PREFS = "settings/FETCH";
+const FETCH_PREFS_REQUEST = "settings/FETCH_REQUEST";
+type FETCH_PREFS_REQUEST_TYPE = "settings/FETCH_REQUEST";
+const FETCH_PREFS_SUCCESS = "settings/FETCH_SUCCESS";
+type FETCH_PREFS_SUCCESS_TYPE = "settings/FETCH_SUCCESS";
+const FETCH_PREFS_FAILURE = "settings/FETCH_FAILURE";
+type FETCH_PREFS_FAILURE_TYPE = "settings/FETCH_FAILURE";
+const FETCH_PREFS_CACHE = "settings/FETCH_CACHE";
+type FETCH_PREFS_CACHE_TYPE = "settings/FETCH_CACHE";
 
-export const SET_PREF = "settings/SET_PREF";
-const SET_PREF_REQUEST = `${SET_PREF}_REQUEST`;
-const SET_PREF_SUCCESS = `${SET_PREF}_SUCCESS`;
-const SET_PREF_FAILURE = `${SET_PREF}_FAILURE`;
-const SET_PREF_NO_CHANGE = `${SET_PREF}_NO_CHANGE`;
+type FetchRequestAction = { type: FETCH_PREFS_REQUEST_TYPE };
+type FetchSuccessAction = {
+  type: FETCH_PREFS_SUCCESS_TYPE,
+  // data is passed from the server in this format, coerce it into PrefsType
+  preferences: Array<{ key: string, value: PrefValue }>
+};
+type FetchFailureAction = {
+  type: FETCH_PREFS_FAILURE_TYPE,
+  errorMessage: string,
+  meta: Object
+};
+type FetchCacheAction = { type: FETCH_PREFS_CACHE_TYPE };
 
-export const FETCH_SCHEMA = "settings/FETCH_SCHEMA";
-const FETCH_SCHEMA_REQUEST = `${FETCH_SCHEMA}_REQUEST`;
-const FETCH_SCHEMA_SUCCESS = `${FETCH_SCHEMA}_SUCCESS`;
-const FETCH_SCHEMA_FAILURE = `${FETCH_SCHEMA}_FAILURE`;
-const FETCH_SCHEMA_CACHE = `${FETCH_SCHEMA}_CACHE`;
+const SET_PREF_REQUEST = "settings/SET_PREF_REQUEST";
+type SET_PREF_REQUEST_TYPE = "settings/SET_PREF_REQUEST";
+const SET_PREF_SUCCESS = "settings/SET_PREF_SUCCESS";
+type SET_PREF_SUCCESS_TYPE = "settings/SET_PREF_SUCCESS";
+const SET_PREF_FAILURE = "settings/SET_PREF_FAILURE";
+type SET_PREF_FAILURE_TYPE = "settings/SET_PREF_FAILURE";
+const SET_PREF_NO_CHANGE = "settings/SET_PREF_NO_CHANGE";
+type SET_PREF_NO_CHANGE_TYPE = "settings/SET_PREF_NO_CHANGE";
+
+type SetPrefRequestAction = {
+  type: SET_PREF_REQUEST_TYPE,
+  key: string,
+  newVal: PrefValue
+};
+type SetPrefSuccessAction = { type: SET_PREF_SUCCESS_TYPE };
+type SetPrefFailureAction = {
+  type: SET_PREF_FAILURE_TYPE,
+  errorMessage: string,
+  meta: Object
+};
+type SetPrefNoChangeAction = { type: SET_PREF_NO_CHANGE_TYPE };
+
+const FETCH_SCHEMA = "settings/FETCH_SCHEMA";
+const FETCH_SCHEMA_REQUEST = "settings/FETCH_SCHEMA_REQUEST";
+type FETCH_SCHEMA_REQUEST_TYPE = "settings/FETCH_SCHEMA_REQUEST";
+const FETCH_SCHEMA_SUCCESS = "settings/FETCH_SCHEMA_SUCCESS";
+type FETCH_SCHEMA_SUCCESS_TYPE = "settings/FETCH_SCHEMA_SUCCESS";
+const FETCH_SCHEMA_FAILURE = "settings/FETCH_SCHEMA_FAILURE";
+type FETCH_SCHEMA_FAILURE_TYPE = "settings/FETCH_SCHEMA_FAILURE";
+const FETCH_SCHEMA_CACHE = "settings/FETCH_SCHEMA_CACHE";
+type FETCH_SCHEMA_CACHE_TYPE = "settings/FETCH_SCHEMA_CACHE";
+
+type FetchSchemaRequestAction = { type: FETCH_SCHEMA_REQUEST_TYPE };
+type FetchSchemaSuccessAction = {
+  type: FETCH_SCHEMA_SUCCESS_TYPE,
+  schema: SchemaType
+};
+type FetchSchemaFailureAction = {
+  type: FETCH_SCHEMA_FAILURE_TYPE,
+  errorMessage: string,
+  meta: Object
+};
+type FetchSchemaCacheAction = { type: FETCH_SCHEMA_CACHE_TYPE };
 
 // ================================================================================
 // Reducers
 // ================================================================================
-export type PrefValue = ?(string | Array<string> | number | boolean);
-export type PrefsType = { +[key: string]: PrefValue }; // Preferences stored in 'key: value' format
-
-export type State = {
+type State = {
   schema: ?SchemaType,
   prefs: PrefsType,
   allPrefsFetched: boolean // Whether or not all preferences have been fetched, required as prefs can be fetch individually
 };
+
+type Action =
+  | FetchRequestAction
+  | FetchSuccessAction
+  | FetchFailureAction
+  | FetchCacheAction
+  | SetPrefRequestAction
+  | SetPrefSuccessAction
+  | SetPrefFailureAction
+  | SetPrefNoChangeAction
+  | FetchSchemaRequestAction
+  | FetchSchemaSuccessAction
+  | FetchSchemaFailureAction
+  | FetchSchemaCacheAction;
 
 export default function settingsReducers(
   state: State = {
@@ -43,7 +107,7 @@ export default function settingsReducers(
     prefs: {},
     allPrefsFetched: false
   },
-  action: Object = {}
+  action: Action
 ): State {
   switch (action.type) {
     case FETCH_PREFS_SUCCESS:
@@ -96,12 +160,12 @@ export const selectIsSettingsLoading = createLoadingSelector([
   FETCH_SCHEMA
 ]);
 
-export const selectIsSettingsLoaded = (state): boolean =>
+export const selectIsSettingsLoaded = (state: GlobalState) =>
   state.settings.allPrefsFetched;
 
-export const selectSettingsSchema = (state): ?SchemaType =>
+export const selectSettingsSchema = (state: GlobalState) =>
   state.settings.schema;
-export const selectSettingsPrefs = (state): PrefsType => state.settings.prefs;
+export const selectSettingsPrefs = (state: GlobalState) => state.settings.prefs;
 
 // [Written 5/10/2019] default viewer could be missing from the prefs object
 // also, there is no typing for prefs currently, so manually typing this
@@ -109,18 +173,29 @@ export type DefaultViewer =
   | "left_to_right"
   | "right_to_left"
   | "webtoon"
+  | null
   | void;
-export const selectDefaultViewer = (state): DefaultViewer =>
+export const selectDefaultViewer = (state: GlobalState): DefaultViewer =>
   state.settings.prefs.pref_default_viewer_key;
 
 // ================================================================================
 // Action Creators
 // ================================================================================
+type GetState = () => GlobalState;
+type PromiseAction = Promise<Action>;
+// eslint-disable-next-line no-use-before-define
+type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
+type Dispatch = (
+  action: Action | ThunkAction | PromiseAction | Array<Action>
+) => any;
+
 type FetchOptions = { ignoreCache?: boolean };
 
-export function fetchSettings({ ignoreCache = false }: FetchOptions = {}) {
-  return (dispatch: Function, getState: Function) => {
-    if (!ignoreCache && getState().settings.allPrefsFetched) {
+export function fetchSettings({
+  ignoreCache = false
+}: FetchOptions = {}): ThunkAction {
+  return (dispatch, getState) => {
+    if (!ignoreCache && selectIsSettingsLoaded(getState())) {
       return Promise.resolve(dispatch({ type: FETCH_PREFS_CACHE }));
     }
 
@@ -140,8 +215,9 @@ export function fetchSettings({ ignoreCache = false }: FetchOptions = {}) {
   };
 }
 
-export function setSetting(key: string, newValue: PrefValue) {
-  return (dispatch: Function, getState: Function) => {
+export function setSetting(key: string, newValue: PrefValue): ThunkAction {
+  return (dispatch, getState) => {
+    // TODO: replace this getState() check with a selector
     if (getState().settings.prefs[key] === newValue)
       return Promise.resolve(dispatch({ type: SET_PREF_NO_CHANGE }));
 
@@ -150,7 +226,7 @@ export function setSetting(key: string, newValue: PrefValue) {
     return Server.api()
       .setPreference(key, { value: newValue })
       .then(
-        () => dispatch({ type: SET_PREF_SUCCESS, key, newVal: newValue }),
+        () => dispatch({ type: SET_PREF_SUCCESS }),
         error =>
           dispatch({
             type: SET_PREF_FAILURE,
@@ -163,15 +239,10 @@ export function setSetting(key: string, newValue: PrefValue) {
 
 export function fetchSettingsSchema({
   ignoreCache = false
-}: FetchOptions = {}) {
-  return (dispatch: Function, getState: Function) => {
-    if (!ignoreCache && getState().settings.schema)
-      return Promise.resolve(
-        dispatch({
-          type: FETCH_SCHEMA_CACHE,
-          schema: getState().settings.schema
-        })
-      );
+}: FetchOptions = {}): ThunkAction {
+  return (dispatch, getState) => {
+    if (!ignoreCache && selectSettingsSchema(getState()))
+      return Promise.resolve(dispatch({ type: FETCH_SCHEMA_CACHE }));
 
     dispatch({ type: FETCH_SCHEMA_REQUEST });
 
