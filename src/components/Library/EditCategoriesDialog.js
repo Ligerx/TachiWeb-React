2e889b1276dfc03cca3b29c9d20fd58e14ffb92c
Alex Wang <ipoopfool@gmail.com>
2019-06-24 22:01:32 -0700

    more wip on categories dialog

--- src/components/Library/EditCategoriesDialog.js
+++ src/components/Library/EditCategoriesDialog.js
@@ -2,25 +2,16 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
 import Icon from "@material-ui/core/Icon";
-import Tooltip from "@material-ui/core/Tooltip";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
-import Avatar from "@material-ui/core/Avatar";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemText from "@material-ui/core/ListItemText";
+import DialogActions from "@material-ui/core/DialogActions";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import { selectCategories } from "redux-ducks/categories";
-import DialogContent from "@material-ui/core/DialogContent";
 
 type Props = {
   isOpen: boolean,
@@ -33,30 +24,29 @@ const EditCategoriesDialog = ({ isOpen, onClose }: Props) => {
   const categories = useSelector(selectCategories);
 
   return (
-    <Dialog open={isOpen} onClose={onClose}>
+    <Dialog open={isOpen} onClose={onClose} fullWidth>
       <DialogTitle>Edit Categories</DialogTitle>
-      {/* <DialogContent> */}
       <List>
         {categories.map(category => (
           <ListItem key={category.id}>
             <ListItemIcon>
               <Icon>drag_handle</Icon>
             </ListItemIcon>
-            <ListItemText primary={category.name} />
+            <TextField
+              value={category.name}
+              inputProps={{ "aria-label": "bare" }}
+              fullWidth
+              variant="outlined"
+              margin="dense"
+            />
           </ListItem>
-          // <ListItem>
-          // <Grid container spacing={1}>
-          //   <Grid item>
-          //     <Icon>drag_handle</Icon>
-          //   </Grid>
-          //   <Grid item>
-          //     <TextField />
-          //   </Grid>
-          // </Grid>
-          // </ListItem>
         ))}
       </List>
-      {/* </DialogContent> */}
+      <DialogActions>
+        <Button onClick={() => {}} color="primary">
+          Add Category
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
