4f54a766b2e6145964422db4bec04c9db156fc6b
Alex Wang <ipoopfool@gmail.com>
2019-06-27 20:42:28 -0700

    custom hook for derived state UNTESTED

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -1,12 +1,22 @@
 // @flow
-import { useRef, useEffect } from "react";
+import { useState, useEffect, useRef, useEffect } from "react";
 
 // https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
-// eslint-disable-next-line import/prefer-default-export
 export function usePrevious<T>(value: T): T {
   const ref = useRef();
   useEffect(() => {
     ref.current = value;
   });
   return ref.current;
+}
+
+// https://stackoverflow.com/questions/54625831/how-to-sync-props-to-state-using-react-hook-setstate
+export function useDerivedStateFromProps<T>(props: T): [T, (state: T) => void] {
+  const [state, setState] = useState(props);
+
+   useEffect(() => {
+       setState(props);
+   }, [props]);
+
+   return [state, setState];
 }
