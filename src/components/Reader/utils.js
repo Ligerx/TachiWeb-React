2253b6d67b288285ca5a9be86b6e5e400e98ecf6
Alex Wang <ipoopfool@gmail.com>
2019-08-14 21:34:36 -0700

    Change reading status update logic. Minor other changes.

--- src/components/Reader/utils.js
+++ src/components/Reader/utils.js
@@ -4,7 +4,6 @@ import { useDispatch } from "react-redux";
 import { Server } from "api";
 import { updateReadingStatus } from "redux-ducks/chapters/actionCreators";
 
-// eslint-disable-next-line import/prefer-default-export
 export function usePagePreloader(
   mangaId: number,
   chapterId: number,
@@ -12,16 +11,13 @@ export function usePagePreloader(
   pageCount: number,
   nextChapterId: ?number
 ) {
-  // This expects whatever component is using it to load pageCount for the current chapter
-
-  // TODO: Do we want to preload one behind as well?
-  //       Loading from only this chapter would be trivial.
-  //
-  //       However, if you want to load from the previous chapter, it would
-  //       require the previous chapter object and prev chapter pageCount.
-
   // https://www.photo-mark.com/notes/image-preloading/
   // https://stackoverflow.com/questions/1787319/preload-hidden-css-images?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+  // TODO: Do we want to preload one previous page as well?
+  // It would be easy to load pages from the current chapter.
+  // However, load one page from the previous chapter would require
+  // prevChapterId and prev chapter pageCount.
 
   useEffect(() => {
     const numPreloadAhead = 3;
@@ -36,7 +32,6 @@ export function usePagePreloader(
       } else if (nextChapterId) {
         // Load pages from next chapter
         // NOTE: Not bothering to check next chapter's pageCount. Doubt this will be a problem.
-        /* eslint-disable no-mixed-operators */
         image.src = Server.image(mangaId, nextChapterId, page + i - pageCount);
       }
     }
@@ -48,16 +43,6 @@ export function useUpdateReadingStatus(
   chapterId: number,
   page: number
 ) {
-  // TODO: special case to consider?
-  //       If you are on the last page of a chapter then go to the next chapter's first page,
-  //       AND we didn't already mark you as having completed this chapter,
-  //       your reading status currently won't be updated
-  //
-  //       Could this also cause problems if a chapter only has 1 page total?
-
-  // TODO: should I bother ignoring page jumps? (within the same chapter)
-  //       One method of doing so is if the difference in page change isn't 1, ignore it
-
   const dispatch = useDispatch();
 
   useEffect(() => {
