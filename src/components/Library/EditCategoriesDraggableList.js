1f808d1b722eb1298f1879934970c2ecbe605131
Alex Wang <ipoopfool@gmail.com>
2019-09-03 10:28:04 -0700

    Add dispatch logic to the drag component

--- src/components/Library/EditCategoriesDraggableList.js
+++ src/components/Library/EditCategoriesDraggableList.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import EditCategoriesList from "components/Library/EditCategoriesList";
+import { reorderCategory } from "redux-ducks/categories/actionCreators";
 
 // Following the functional component example in the react-beautiful-dnd docs
 // https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/about/examples.md
@@ -10,7 +11,12 @@ import EditCategoriesList from "components/Library/EditCategoriesList";
 const EditCategoriesDraggableList = () => {
   const dispatch = useDispatch();
 
-  const handleDragEnd = () => {};
+  const handleDragEnd = result => {
+    if (!result.destination) return;
+    if (result.destination.index === result.source.index) return;
+
+    dispatch(reorderCategory(result.source.index, result.destination.index));
+  };
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
