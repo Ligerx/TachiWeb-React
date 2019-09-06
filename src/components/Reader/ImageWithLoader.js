0d60165286d9f62b04f3c8bf9de562c7ff026c6e
Alex Wang <ipoopfool@gmail.com>
2019-09-05 18:51:31 -0700

    WIP updating ImageWithLoader

--- src/components/Reader/ImageWithLoader.js
+++ src/components/Reader/ImageWithLoader.js
@@ -1,9 +1,9 @@
 // @flow
-import React, { Component } from "react";
+import React, { useState } from "react";
 import CenteredLoading from "components/Loading/CenteredLoading";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/styles";
 
 // https://www.javascriptstuff.com/detect-image-load/
 
@@ -19,114 +19,72 @@ import { withStyles } from "@material-ui/core/styles";
 //      It instead shows the image error placeholder as if it failed.
 //      I'm guessing React is confused that it's image (in cache) changed, but the src/key didn't.
 
-const styles = {
-  verticallyCenter: {
+const useStyles = makeStyles({
+  placeholder: {
+    height: "105vh",
+
+    // vertically center children
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  img: {
+    width: "100%"
   }
-};
+});
 
 type Props = {
-  classes: Object, // injected styles (for this component)
-  src: ?string, // if src == null, <img> will not render
-  alt: string,
-  notLoadedHeight?: number | string // any valid height
+  src: string,
+  alt: string // requiring alt so eslint doesn't yell at me
 }; // extra props will be passed to <img>
 
-type State = {
-  status: "LOADING" | "LOADED" | "FAILED",
-  retries: number
-};
+type StatusType = "LOADING" | "LOADED" | "FAILED";
 
-class ImageWithLoader extends Component<Props, State> {
-  static defaultProps = {
-    notLoadedHeight: "105vh"
-  };
+const ImageWithLoader = ({ src, alt, ...otherProps }: Props) => {
+  const classes = useStyles();
 
-  state = {
-    status: "LOADING",
-    retries: 0
-  };
+  const [status, setStatus] = useState<StatusType>("LOADING");
+  const [retries, setRetries] = useState(0);
 
-  /* eslint-disable react/no-did-update-set-state */
-  componentDidUpdate(prevProps: Props) {
-    // Changing the img src doesn't trigger any event to update
-    // status so you have to do it manually.
-    if (prevProps.src !== this.props.src) {
-      this.setState({
-        status: "LOADING",
-        retries: 0
-      });
-    }
-  }
-
-  handleImageLoad = () => this.setState({ status: "LOADED" });
-
-  handleImageError = () => this.setState({ status: "FAILED" });
-
-  handleRetryClick = () => {
-    const { src } = this.props;
-    if (!src) return;
+  const handleImageLoad = () => setStatus("LOADED");
+  const handleImageError = () => setStatus("FAILED");
+  const handleRetryClick = () => {
+    setStatus("LOADING");
+    setRetries(prevRetries => prevRetries + 1);
 
     // https://stackoverflow.com/questions/23160107/javascript-how-to-retry-loading-an-image-without-appending-query-string
-    this.setState(prevState => ({
-      status: "LOADING",
-      retries: prevState.retries + 1
-    }));
-
     const img = new Image();
-    img.onload = this.handleImageLoad;
-    img.onerror = this.handleImageError;
+    img.onload = handleImageLoad;
+    img.onerror = handleImageError;
     img.src = src;
   };
 
-  render() {
-    const { classes, src, alt, notLoadedHeight, ...otherProps } = this.props;
-    const { status, retries } = this.state;
+  return (
+    <>
+      {/* img should occupy no space before it loads */}
+      <img
+        {...otherProps}
+        className={classes.img}
+        onLoad={handleImageLoad}
+        onError={handleImageError}
+        src={src}
+        alt={alt}
+        key={`${src}-${retries}`}
+      />
 
-    const imgStyles = {
-      width: "100%",
-      // Only show when image is loaded
-      display: status === "LOADED" ? "block" : "none"
-    };
-
-    return (
-      <>
-        {src && (
-          <img
-            {...otherProps}
-            onLoad={this.handleImageLoad}
-            onError={this.handleImageError}
-            src={src}
-            alt={alt}
-            style={imgStyles}
-            key={`${src}-${retries}`}
-          />
-        )}
-
-        {status === "LOADING" && (
-          <div
-            className={classes.verticallyCenter}
-            style={{ height: notLoadedHeight }}
-          >
-            <CenteredLoading />
-          </div>
-        )}
-        {status === "FAILED" && (
-          <div
-            className={classes.verticallyCenter}
-            style={{ height: notLoadedHeight }}
-          >
-            <Button variant="contained" onClick={this.handleRetryClick}>
+      {(status === "LOADING" || status === "FAILED") && (
+        <div className={classes.placeholder}>
+          {status === "LOADING" && <CenteredLoading />}
+          {status === "FAILED" && (
+            <Button variant="contained" onClick={handleRetryClick}>
               <Icon>refresh</Icon>
               Retry
             </Button>
-          </div>
-        )}
-      </>
-    );
-  }
-}
+          )}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default withStyles(styles)(ImageWithLoader);
+export default ImageWithLoader;
