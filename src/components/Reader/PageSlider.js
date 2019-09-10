2fbe42b6bd1aa9dfbb029a6ad3ccc2c901a63cc3
Alex Wang <ipoopfool@gmail.com>
2019-09-09 22:10:15 -0700

    Make PageSlider stop changing sliderValue as page changes, use makeStyles, update prop types

--- src/components/Reader/PageSlider.js
+++ src/components/Reader/PageSlider.js
@@ -1,35 +1,9 @@
 // @flow
-import React, { useState, useEffect } from "react";
-import Typography from "@material-ui/core/Typography";
+import React, { useState, useEffect, useRef } from "react";
 import "rc-slider/assets/index.css";
 import Slider, { createSliderWithTooltip } from "rc-slider";
-import { withStyles } from "@material-ui/core/styles";
-
-const SliderWithTooltip = createSliderWithTooltip(Slider);
-
-// need whiteSpace: 'pre' so it doesn't wrap. rc-slider's width was forcing them to be too small
-const marginSlider = 24;
-const marginButton = 8;
-const styles = {
-  leftText: {
-    whiteSpace: "pre",
-    marginLeft: marginButton,
-    marginRight: marginSlider
-  },
-  rightText: {
-    whiteSpace: "pre",
-    marginLeft: marginSlider,
-    marginRight: marginButton
-  }
-};
-
-type Props = {
-  classes: Object,
-
-  pageCount: number,
-  page: number,
-  onJumpToPage: Function
-};
+import { makeStyles } from "@material-ui/styles";
+import Typography from "@material-ui/core/Typography";
 
 // Using sliderValue state so slider can be dragged around (which updates the state onChange)
 // If you use props.page, the slider value can't be changed
@@ -41,15 +15,62 @@ type Props = {
 //        ReaderOverlay has a z-index, which is interfering with the tooltip.
 //        Ideally, this CSS wouldn't be necessary
 
-// FIXME: Not sure if it's due to PageSlider, WebtoonReader, or Reader. But jumping pages
-//        with the slider seems slow. And going FORWARD makes the url go through every
-//        page individually. It doesn't seem to be breaking WebtoonReader pagesToLoad[] though.
+// FIXME: For some reason, the slider seems to shift around a bit. It seems to be happening
+//        most frequently when jumping backwards a large number of pages. Not really sure
+//        why, but it's also only a cosmetic bug.
 
-const PageSlider = ({ classes, pageCount, page, onJumpToPage }: Props) => {
+type Props = {
+  pageCount: number,
+  page: number,
+  onJumpToPage: number => any
+};
+
+const SliderWithTooltip = createSliderWithTooltip(Slider);
+
+// need whiteSpace: 'pre' so it doesn't wrap. rc-slider's width was forcing them to be too small
+const marginSlider = 24;
+const marginButton = 8;
+const useStyles = makeStyles({
+  leftText: {
+    whiteSpace: "pre",
+    marginLeft: marginButton,
+    marginRight: marginSlider
+  },
+  rightText: {
+    whiteSpace: "pre",
+    marginLeft: marginSlider,
+    marginRight: marginButton
+  }
+});
+
+const PageSlider = ({ pageCount, page, onJumpToPage }: Props) => {
+  // Using an intermediate value that tracks page so that you can drag the
+  // slider without jumping until mouse up.
   const [sliderValue, setSliderValue] = useState(page);
+
+  // Keep an internal value that remember if we are currently jumping pages.
+  // This is used to prevent the sliderValue from returning to the current page
+  // and then slowly changing value as the page changes.
+  const isJumpingRef = useRef(false);
+
   useEffect(() => {
-    setSliderValue(page);
+    if (!isJumpingRef.current) {
+      setSliderValue(page);
+    }
   }, [page]);
+
+  useEffect(() => {
+    if (isJumpingRef.current && page === sliderValue) {
+      isJumpingRef.current = false;
+    }
+  }, [page, sliderValue]);
+
+  const handleAfterChange = (value: number) => {
+    isJumpingRef.current = true;
+    onJumpToPage(value - 1);
+  };
+
+  const classes = useStyles();
 
   return (
     <>
@@ -59,7 +80,7 @@ const PageSlider = ({ classes, pageCount, page, onJumpToPage }: Props) => {
         max={pageCount}
         value={sliderValue + 1}
         onChange={(value: number) => setSliderValue(value - 1)}
-        onAfterChange={(value: number) => onJumpToPage(value - 1)}
+        onAfterChange={handleAfterChange}
         tipFormatter={value => `Page ${value}`}
       />
       <Typography className={classes.rightText}>{pageCount}</Typography>
@@ -67,4 +88,4 @@ const PageSlider = ({ classes, pageCount, page, onJumpToPage }: Props) => {
   );
 };
 
-export default withStyles(styles)(PageSlider);
+export default PageSlider;
