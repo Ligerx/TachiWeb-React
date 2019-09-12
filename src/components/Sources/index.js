18ecfe990f6f921c1ac7d9e77bb65a656112a724
Alex Wang <ipoopfool@gmail.com>
2019-09-11 22:51:36 -0700

    Display languages and source names

--- src/components/Sources/index.js
+++ src/components/Sources/index.js
@@ -1,19 +1,24 @@
 // @flow
 import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Helmet } from "react-helmet";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Container from "@material-ui/core/Container";
 import { Client } from "api";
 import FullScreenLoading from "components/Loading/FullScreenLoading";
-import ResponsiveGrid from "components/ResponsiveGrid";
 import BackButton from "components/BackButton";
-import { useSelector, useDispatch } from "react-redux";
-import { selectSources } from "redux-ducks/sources";
+import { langPrettyPrint } from "components/utils";
+import {
+  selectSourcesByLanguage,
+  selectSourceLanguages
+} from "redux-ducks/sources";
 import { fetchSources } from "redux-ducks/sources/actionCreators";
 
 const Sources = () => {
-  const sources = useSelector(selectSources);
+  const sourceLanguages = useSelector(selectSourceLanguages);
+  const sourcesByLanguage = useSelector(selectSourcesByLanguage);
 
   const dispatch = useDispatch();
 
@@ -35,7 +40,16 @@ const Sources = () => {
         </Toolbar>
       </AppBar>
 
-      <ResponsiveGrid maxWidth="xs">{JSON.stringify(sources)}</ResponsiveGrid>
+      <Container>
+        {sourceLanguages.map(lang => (
+          <>
+            <Typography variant="h5">{langPrettyPrint(lang)}</Typography>
+            {sourcesByLanguage[lang].map(source => (
+              <div>{source.name}</div>
+            ))}
+          </>
+        ))}
+      </Container>
 
       {/* {isExtensionsLoading && <FullScreenLoading />} */}
     </>
