282fae3424f9501110d1075716c75923a4439742
Alex Wang <ipoopfool@gmail.com>
2019-09-12 20:34:33 -0700

    Implement hiding sources

--- src/components/Sources/SourceRow.js
+++ src/components/Sources/SourceRow.js
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import type { Source } from "@tachiweb/api-client";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import { updateHiddenSources } from "redux-ducks/settings/actionCreators";
 
 type Props = {
   source: Source,
@@ -13,10 +14,14 @@ type Props = {
 const SourceRow = ({ source, isEnabled }: Props) => {
   const dispatch = useDispatch();
 
+  const handleChange = event => {
+    dispatch(updateHiddenSources(source.id, !event.target.checked));
+  };
+
   return (
     <>
       <FormControlLabel
-        control={<Checkbox checked={isEnabled} onChange={() => {}} />}
+        control={<Checkbox checked={isEnabled} onChange={handleChange} />}
         label={source.name}
       />
     </>
