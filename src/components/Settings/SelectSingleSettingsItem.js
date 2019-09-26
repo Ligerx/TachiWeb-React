471e746d1d3df8d6dd8360f11c407450f219ae7d
Alex Wang <ipoopfool@gmail.com>
2019-09-25 21:34:15 -0700

    convert SelectSingleSettingsItem to function

--- src/components/Settings/SelectSingleSettingsItem.js
+++ src/components/Settings/SelectSingleSettingsItem.js
@@ -1,5 +1,5 @@
 // @flow
-import * as React from "react";
+import React, { useState, useRef } from "react";
 import type { BaseSettingsItemProps } from "components/Settings/SettingsItem";
 import { currentValueOrFallback } from "components/Settings/SettingsItem";
 import type { SelectSingleSchemaEntry } from "types/settings-schema";
@@ -13,10 +13,7 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
 import SettingsListItem from "components/Settings/SettingsListItem";
 
-type State = {
-  dialogOpen: boolean, // Whether or not the editing dialog is open
-  localValue: string // The displayed value of this preference (not committed to store or server)
-};
+type Props = BaseSettingsItemProps & { schema: SelectSingleSchemaEntry };
 
 /**
  * A preference allowing the user to select exactly one value out of a list of choices
@@ -24,98 +21,87 @@ type State = {
  * When editing this preference, a dialog is opened. The user can modify the value of the preference in the dialog
  * and the modified value is not committed to the store or server until the user confirms it.
  */
-class SelectSingleSettingsItem extends React.Component<
-  BaseSettingsItemProps & { schema: SelectSingleSchemaEntry },
-  State
-> {
-  state: State = {
-    dialogOpen: false,
-    localValue: currentValueOrFallback(this.props)
-  };
+const SelectSingleSettingsItem = (props: Props) => {
+  const { schema, onUpdateSetting } = props;
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [localValue, setLocalValue] = useState(currentValueOrFallback(props));
+
+  const radioGroupRef = useRef(null);
 
   // Reset local state & open dialog
-  handleClick = () =>
-    this.setState(state => ({
-      ...state,
-      localValue: currentValueOrFallback(this.props),
-      dialogOpen: true
-    }));
-
-  handleLocalChange = (_: any, newValue: string) =>
-    this.setState(state => ({ ...state, localValue: newValue }));
-
-  handleDialogShow = () => {
-    if (this.radioGroupRef != null) this.radioGroupRef.focus();
+  const handleClick = () => {
+    setLocalValue(currentValueOrFallback(props));
+    setDialogOpen(true);
   };
 
-  closeDialog = () => this.setState(state => ({ ...state, dialogOpen: false }));
+  const handleLocalChange = (_: any, newValue: string) => {
+    setLocalValue(newValue);
+  };
 
-  // Just close dialog, do not push changes to server
-  handleCancel = () => this.closeDialog();
+  const handleDialogShow = () => {
+    if (radioGroupRef.current != null) radioGroupRef.current.focus();
+  };
 
-  handleOk = () => {
-    const { onUpdateSetting, schema } = this.props;
-    const { localValue } = this.state;
-    this.closeDialog();
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
 
+  const handleOk = () => {
     // Push changes to server
     onUpdateSetting(schema.key, localValue);
+    closeDialog();
   };
 
-  radioGroupRef: ?HTMLInputElement;
+  const current = schema.choices.find(
+    it => it.id === currentValueOrFallback(props)
+  );
 
-  render() {
-    const { schema } = this.props;
-    const { dialogOpen, localValue } = this.state;
+  return (
+    <>
+      <SettingsListItem
+        onClick={handleClick}
+        selectedValueLabel={current != null ? current.label : null}
+        schema={schema}
+      />
 
-    const current = schema.choices.find(
-      it => it.id === currentValueOrFallback(this.props)
-    );
-    return (
-      <>
-        <SettingsListItem
-          onClick={this.handleClick}
-          selectedValueLabel={current != null ? current.label : null}
-          schema={schema}
-        />
-        <Dialog
-          disableBackdropClick
-          disableEscapeKeyDown
-          maxWidth="xs"
-          open={dialogOpen}
-          onEntering={this.handleDialogShow}
-        >
-          <DialogTitle>{schema.label}</DialogTitle>
-          <DialogContent>
-            <RadioGroup
-              ref={ref => {
-                this.radioGroupRef = ref;
-              }}
-              value={localValue}
-              onChange={this.handleLocalChange}
-            >
-              {schema.choices.map(({ id, label }) => (
-                <FormControlLabel
-                  value={id}
-                  key={id}
-                  control={<Radio />}
-                  label={label}
-                />
-              ))}
-            </RadioGroup>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleCancel} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleOk} color="primary">
-              Ok
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </>
-    );
-  }
-}
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        maxWidth="xs"
+        open={dialogOpen}
+        onEntering={handleDialogShow}
+      >
+        <DialogTitle>{schema.label}</DialogTitle>
+
+        <DialogContent>
+          <RadioGroup
+            ref={radioGroupRef}
+            value={localValue}
+            onChange={handleLocalChange}
+          >
+            {schema.choices.map(({ id, label }) => (
+              <FormControlLabel
+                key={id}
+                value={id}
+                control={<Radio />}
+                label={label}
+              />
+            ))}
+          </RadioGroup>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={closeDialog} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleOk} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
 
 export default SelectSingleSettingsItem;
