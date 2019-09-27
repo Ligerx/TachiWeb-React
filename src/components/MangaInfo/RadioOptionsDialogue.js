8935126b67abfca7d6cfae95f9382d6011642358
Alex Wang <ipoopfool@gmail.com>
2019-09-26 18:35:52 -0700

    convert RadioOptionsDialogue to function

--- src/components/MangaInfo/RadioOptionsDialogue.js
+++ src/components/MangaInfo/RadioOptionsDialogue.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import RadioGroup from "@material-ui/core/RadioGroup";
@@ -21,79 +21,55 @@ type Props = {
   onClose: Function
 };
 
-type State = {
-  // Keep a local copy of value for the user to edit
-  value: string
-};
+const RadioOptionsDialogue = ({
+  title,
+  open,
+  value,
+  options,
+  onClose
+}: Props) => {
+  const [localValue, setLocalValue] = useState(value);
 
-class RadioOptionsDialogue extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+  const radioGroup = useRef(null);
 
-    this.state = {
-      value: this.props.value
-    };
-  }
-
-  radioGroup = null;
-
-  handleEntering = () => {
-    this.setState({ value: this.props.value }); // reset state on open
-    if (this.radioGroup) {
-      this.radioGroup.focus();
+  const handleEntering = () => {
+    setLocalValue(value); // reset state on open
+    if (radioGroup.current) {
+      radioGroup.current.focus();
     }
   };
 
-  handleCancel = () => {
-    this.props.onClose();
+  const handleOk = () => {
+    onClose(localValue);
   };
 
-  handleOk = () => {
-    this.props.onClose(this.state.value);
+  const handleChange = (event: SyntheticEvent<>, newValue: string) => {
+    setLocalValue(newValue);
   };
 
-  handleChange = (event: SyntheticEvent<>, value: string) => {
-    this.setState({ value });
-  };
+  return (
+    <Dialog open={open} onEntering={handleEntering} onClose={onClose}>
+      <DialogTitle>{title}</DialogTitle>
 
-  render() {
-    const { title, open, options } = this.props;
-    const { value } = this.state;
+      <DialogContent>
+        <RadioGroup ref={radioGroup} value={value} onChange={handleChange}>
+          {options.map(option => (
+            <FormControlLabel
+              value={option.flagState}
+              key={option.flagState}
+              control={<Radio />}
+              label={option.label}
+            />
+          ))}
+        </RadioGroup>
+      </DialogContent>
 
-    return (
-      <Dialog
-        open={open}
-        onEntering={this.handleEntering}
-        onClose={this.handleCancel}
-      >
-        <DialogTitle>{title}</DialogTitle>
-
-        <DialogContent>
-          <RadioGroup
-            ref={node => {
-              this.radioGroup = node;
-            }}
-            value={value}
-            onChange={this.handleChange}
-          >
-            {options.map(option => (
-              <FormControlLabel
-                value={option.flagState}
-                key={option.flagState}
-                control={<Radio />}
-                label={option.label}
-              />
-            ))}
-          </RadioGroup>
-        </DialogContent>
-
-        <DialogActions>
-          <Button onClick={this.handleCancel}>Cancel</Button>
-          <Button onClick={this.handleOk}>Select</Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
-}
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={handleOk}>Select</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default RadioOptionsDialogue;
