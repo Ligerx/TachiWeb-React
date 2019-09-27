7fa7c1a791436c45ee30620aac0511e554048a9e
Alex Wang <ipoopfool@gmail.com>
2019-09-26 18:25:17 -0700

    convert ChapterMenu to function to use useStyles

--- src/components/MangaInfo/ChapterMenu.js
+++ src/components/MangaInfo/ChapterMenu.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import IconButton from "@material-ui/core/IconButton";
@@ -11,96 +11,86 @@ import classNames from "classnames";
 // Because this is nested inside ChapterListItem, which wraps the entire thing with a Link
 // You have to e.preventDefault() everywhere to stop from navigating to the Reader unintentionally
 
-const useStyles = makeStyles({
-  hiddenMenuItem: { display: "none" }
-});
-
 type Props = {
   chapter: ChapterType,
   toggleRead: Function
 };
 
-type State = { anchorEl: ?HTMLElement };
+const useStyles = makeStyles({
+  hiddenMenuItem: { display: "none" }
+});
 
-class ChapterMenu extends Component<Props, State> {
-  state = {
-    anchorEl: null
+const ChapterMenu = ({ chapter, toggleRead }: Props) => {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event: SyntheticEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setAnchorEl(event.currentTarget);
   };
 
-  handleClick = (event: SyntheticEvent<HTMLButtonElement>) => {
+  const handleClose = (event: SyntheticEvent<>) => {
     event.preventDefault();
-    this.setState({ anchorEl: event.currentTarget });
+    setAnchorEl(null);
   };
 
-  handleClose = (event: SyntheticEvent<>) => {
+  const handleToggleRead = (read: boolean) => (event: SyntheticEvent<>) => {
     event.preventDefault();
-    this.setState({ anchorEl: null });
-  };
-
-  handleToggleRead = (read: boolean) => (event: SyntheticEvent<>) => {
-    event.preventDefault();
-
-    const { toggleRead } = this.props;
     toggleRead(read);
-
-    this.setState({ anchorEl: null });
+    setAnchorEl(null);
   };
 
-  render() {
-    const { classes, chapter } = this.props;
-    const { anchorEl } = this.state;
+  const showMarkAsRead = !chapter.read;
+  const showMarkAsUnread =
+    chapter.read || (!chapter.read && chapter.last_page_read > 0);
 
-    const showMarkAsRead = !chapter.read;
-    const showMarkAsUnread =
-      chapter.read || (!chapter.read && chapter.last_page_read > 0);
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <Icon>more_vert</Icon>
+      </IconButton>
 
-    return (
-      <>
-        <IconButton onClick={this.handleClick}>
-          <Icon>more_vert</Icon>
-        </IconButton>
-
-        {/* getContentAnchorEl must be null to make anchorOrigin work */}
-        <Menu
-          anchorEl={anchorEl}
-          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-          getContentAnchorEl={null}
-          open={Boolean(anchorEl)}
-          onClose={this.handleClose}
-        >
-          {/* Not using the chapter download feature currently */}
-          {/*
+      {/* getContentAnchorEl must be null to make anchorOrigin work */}
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        getContentAnchorEl={null}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {/* Not using the chapter download feature currently */}
+        {/*
           <MenuItem onClick={null}>
             Download
           </MenuItem>
           */}
 
-          <MenuItem
-            onClick={this.handleToggleRead(true)}
-            className={classNames({
-              [classes.hiddenMenuItem]: !showMarkAsRead
-            })}
-          >
-            Mark as Read
-          </MenuItem>
+        <MenuItem
+          onClick={handleToggleRead(true)}
+          className={classNames({
+            [classes.hiddenMenuItem]: !showMarkAsRead
+          })}
+        >
+          Mark as Read
+        </MenuItem>
 
-          <MenuItem
-            onClick={this.handleToggleRead(false)}
-            className={classNames({
-              [classes.hiddenMenuItem]: !showMarkAsUnread
-            })}
-          >
-            Mark as Unread
-          </MenuItem>
+        <MenuItem
+          onClick={handleToggleRead(false)}
+          className={classNames({
+            [classes.hiddenMenuItem]: !showMarkAsUnread
+          })}
+        >
+          Mark as Unread
+        </MenuItem>
 
-          {/* TODO: use the new API for marking multiple chapters as read */}
-          {/*
+        {/* TODO: use the new API for marking multiple chapters as read */}
+        {/*
           <MenuItem onClick={null}>Mark previous as Read</MenuItem>
           */}
-        </Menu>
-      </>
-    );
-  }
-}
+      </Menu>
+    </>
+  );
+};
 
 export default ChapterMenu;
