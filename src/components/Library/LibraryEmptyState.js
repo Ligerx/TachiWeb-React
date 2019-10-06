36662f041b12a1016229b7bfade08db15cbac1eb
Alex Wang <ipoopfool@gmail.com>
2019-10-05 18:00:09 -0700

    scaffolding

---
+++ src/components/Library/LibraryEmptyState.js
@@ -0,0 +1,17 @@
+// @flow
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
+type Props = {};
+
+const LibraryEmptyState = ({  }: Props) => {
+  return (
+    <>
+      <div>TESTING</div>
+    </>
+  );
+};
+
+export default LibraryEmptyState;
