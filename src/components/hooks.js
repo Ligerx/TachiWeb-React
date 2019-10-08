4c916b336f8e085bf3d563da451ff878a048e0ea
Alex Wang <ipoopfool@gmail.com>
2019-10-07 23:24:47 -0700

    Added window and component size hooks

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -1,5 +1,6 @@
 // @flow
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useLayoutEffect, useRef } from "react";
+import throttle from "lodash/throttle";
 
 // https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
 export function usePrevious<T>(value: T): T {
@@ -19,4 +20,71 @@ export function useDerivedStateFromProps<T>(props: T): [T, (state: T) => void] {
   }, [props]);
 
   return [state, setState];
+}
+
+// https://www.hooks.guide/rehooks/useComponentSize
+// Slightly modified and including throttling
+export function useComponentSize(ref) {
+  const [componentSize, setComponentSize] = useState(getSize(ref.current));
+
+  const wait = 100; // arbitrarily picking this wait time
+  const setComponentSizeThrottled = useRef(
+    throttle(size => setComponentSize(size), wait)
+  ).current;
+
+  useLayoutEffect(() => {
+    function handleResize() {
+      if (ref && ref.current) {
+        setComponentSizeThrottled(getSize(ref.current));
+      }
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      setComponentSizeThrottled.cancel();
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [ref, setComponentSizeThrottled]);
+
+  return componentSize;
+}
+// Helper function
+function getSize(el) {
+  if (!el) return {};
+
+  return {
+    width: el.offsetWidth,
+    height: el.offsetHeight
+  };
+}
+
+// mostly based on useComponentSize()
+export function useWindowSize() {
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  const wait = 100; // arbitrarily picking this wait time
+  const setSizeThrottled = useRef(
+    throttle(
+      () =>
+        setSize({
+          width: window.innerWidth,
+          height: window.innerHeight
+        }),
+      wait
+    )
+  ).current;
+
+  useEffect(() => {
+    window.addEventListener("resize", setSizeThrottled);
+
+    return () => {
+      window.removeEventListener("resize", setSizeThrottled);
+    };
+  }, [setSizeThrottled]);
+
+  return size;
 }
