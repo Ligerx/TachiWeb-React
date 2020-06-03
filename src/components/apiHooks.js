fa75ad73d21c2f1e37ad373bf195881d9bf9e16a
Alex Wang <ipoopfool@gmail.com>
2020-06-02 22:16:29 -0700

    write useCategories

--- src/components/apiHooks.js
+++ src/components/apiHooks.js
@@ -2,6 +2,11 @@
 import useSWR from "swr";
 import { useDispatch } from "react-redux";
 import { Server } from "api";
+import type { CategoryType } from "types";
+
+// NOTE: For any calls using the Server.api().{call}, I'm sort of hacking around SWR's intended usage pattern.
+// I'm manually adding a unique key, then using the api call as the fetcher.
+// This is because the api() calls fetch() directly and I don't have access to the url as the key.
 
 function fetcher(url) {
   return fetch(url).then(res => res.json());
@@ -39,4 +44,22 @@ function unreadArrayToMap(
     newUnread[unreadObj.id] = unreadObj.unread;
   });
   return newUnread;
+}
+
+export function useCategories() {
+  const dispatch = useDispatch();
+
+  return useSWR<CategoryType[]>(
+    "/api/v3/categories",
+    Server.api().getCategories,
+    {
+      onError(error) {
+        dispatch({
+          type: "categories/FETCH_FAILURE",
+          errorMessage: "Failed to find your library categories.",
+          meta: { error }
+        });
+      }
+    }
+  );
 }
