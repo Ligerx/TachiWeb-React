6143e1f7fb2187b7e8381d82958dc2c21109f0fa
Alex Wang <ipoopfool@gmail.com>
2020-06-05 15:52:09 -0700

    creat a mangaInfo apiHook file

---
+++ src/apiHooks/mangaInfo.js
@@ -0,0 +1,26 @@
+// @flow
+import useSWR, { mutate } from "swr";
+import { useDispatch } from "react-redux";
+import { Server } from "api";
+import type { Manga, MangaFlags } from "@tachiweb/api-client";
+
+export function useMangaInfo(mangaId: number) {
+  const dispatch = useDispatch();
+
+  return useSWR<Manga>(
+    Server.mangaInfo(mangaId),
+    () => Server.api().getManga(mangaId),
+    {
+      onError(error) {
+        dispatch({
+          type: "mangaInfos/FETCH_FAILURE",
+          errorMessage: "Failed to get this manga's information",
+          meta: { error }
+        });
+      }
+    }
+  );
+}
+
+// TODO remove this placeholder
+export const blah = 0;
