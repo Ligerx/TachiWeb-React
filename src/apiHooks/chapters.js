25a3ce42677d5d6097ea737f92b51576e3fba980
Alex Wang <ipoopfool@gmail.com>
2020-06-05 20:12:40 -0700

    useUpdateChapters

--- src/apiHooks/chapters.js
+++ src/apiHooks/chapters.js
@@ -74,7 +74,35 @@ export function useUpdateReadingStatus(): (
       dispatch({
         type: "chapters/UPDATE_READING_STATUS_FAILURE",
         errorMessage: "Failed to save your reading status",
-        meta: { error }
+        meta: { error, mangaInfo, chapter, readPage, pageCount }
+      });
+    }
+  };
+}
+
+// TODO do I need to be able to support a loading state here?
+/**
+ * Request the server to re-scrape the source site for chapters.
+ * If there have been any changes, re-fetch the cached chapter list from the server.
+ */
+export function useUpdateChapters(): (mangaId: number) => Promise<void> {
+  const dispatch = useDispatch();
+
+  return async mangaId => {
+    try {
+      const response = await fetch(Server.updateMangaChapters(mangaId));
+      const json = await response.json();
+
+      if (!json.success) {
+        throw new Error("json returned success = false");
+      }
+
+      mutate(Server.chapters(mangaId));
+    } catch (error) {
+      dispatch({
+        type: "chapters/UPDATE_FAILURE",
+        errorMessage: "Failed to update the chapters list",
+        meta: { error, mangaId }
       });
     }
   };
