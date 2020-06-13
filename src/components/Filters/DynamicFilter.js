33670aee58edc4f71406b15c75cb06aa32786160
Alex Wang <ipoopfool@gmail.com>
2020-06-12 20:29:31 -0700

    fix memoization of DynamicFilter

--- src/components/Filters/DynamicFilter.js
+++ src/components/Filters/DynamicFilter.js
@@ -8,9 +8,19 @@ import FilterTristate from "components/Filters/FilterTristate";
 import FilterGroup from "components/Filters/FilterGroup";
 import type { FilterAnyType } from "types/filters";
 
-type Props = { filter: FilterAnyType, onChange: FilterAnyType => any };
+type Props = {
+  index: number,
+  filter: FilterAnyType,
+  // remember to memoize this function so that memo works as expected
+  // passing index as well to make it simpler to memoize onChange
+  onChange: (number, FilterAnyType) => any
+};
 
-const DynamicFilter = memo<Props>(({ filter, onChange }: Props) => {
+const DynamicFilter = memo<Props>(({ index, filter, onChange }: Props) => {
+  const handleChange = (newFilter: FilterAnyType) => {
+    onChange(index, newFilter);
+  };
+
   if (["HEADER", "SEPARATOR", "CHECKBOX"].includes(filter._type)) {
     console.error(`Catalogue filters - ${filter._type} is not implemented.`);
     return null;
@@ -18,19 +28,19 @@ const DynamicFilter = memo<Props>(({ filter, onChange }: Props) => {
 
   switch (filter._type) {
     case "TEXT":
-      return <FilterTextField filter={filter} onChange={onChange} />;
+      return <FilterTextField filter={filter} onChange={handleChange} />;
 
     case "SELECT":
-      return <FilterSelect filter={filter} onChange={onChange} />;
+      return <FilterSelect filter={filter} onChange={handleChange} />;
 
     case "SORT":
-      return <FilterSort filter={filter} onChange={onChange} />;
+      return <FilterSort filter={filter} onChange={handleChange} />;
 
     case "TRISTATE":
-      return <FilterTristate filter={filter} onChange={onChange} />;
+      return <FilterTristate filter={filter} onChange={handleChange} />;
 
     case "GROUP":
-      return <FilterGroup filter={filter} onChange={onChange} />;
+      return <FilterGroup filter={filter} onChange={handleChange} />;
 
     default:
       console.error(
--- src/components/Filters/DynamicSourceFilters.js
+++ src/components/Filters/DynamicSourceFilters.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Button from "@material-ui/core/Button";
 import Drawer from "@material-ui/core/Drawer";
 import FormGroup from "@material-ui/core/FormGroup";
@@ -52,16 +52,19 @@ const DynamicSourceFilters = ({
     setFiltersCopy(filters);
   };
 
-  const handleFilterChange = (index: number) => (
-    updatedFilter: FilterAnyType
-  ) => {
-    setFiltersCopy(oldFilters => {
-      return produce(oldFilters, draftFilters => {
-        // eslint-disable-next-line no-param-reassign
-        draftFilters[index] = updatedFilter;
+  // memoized to prevent rerenders of the memo'd DynamicFilter component
+  // requiring that index is passed with updatedFilter instead of via a higher order function to make memoization possible
+  const handleFilterChange = useCallback(
+    (index: number, updatedFilter: FilterAnyType) => {
+      setFiltersCopy(oldFilters => {
+        return produce(oldFilters, draftFilters => {
+          // eslint-disable-next-line no-param-reassign
+          draftFilters[index] = updatedFilter;
+        });
       });
-    });
-  };
+    },
+    []
+  );
 
   if (filters.length === 0) {
     return (
@@ -99,8 +102,9 @@ const DynamicSourceFilters = ({
                 // The order of filters is constant, so using index as the key is fine.
                 // eslint-disable-next-line react/no-array-index-key
                 key={index}
+                index={index}
                 filter={filter}
-                onChange={handleFilterChange(index)}
+                onChange={handleFilterChange}
               />
             ))}
           </FormGroup>
